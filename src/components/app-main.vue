58071fb473ba2ba69030232d1bc7808a1ae729cb
Yura <Yurgeman@users.noreply.github.com>
2022-05-11 14:05:11 +0300

    update buttons style

--- src/components/app-main.vue
+++ src/components/app-main.vue
@@ -74,7 +74,7 @@
         </div>
       </div>
 
-      <div class="tools hstack">
+      <div class="tools hstack" style="padding: .5rem">
         <sea-link
           v-if="state.showSettings"
           @action="doTogglePanel('settings')"
@@ -129,7 +129,7 @@
                   y2="16"></line>
           </svg>
         </sea-link>
-        <div class="-fit">
+        <div class="-fit" style="flex-direction: column;">
           <sea-link
             class="tool"
             :class="{ '-off': state.muteVideo }"
@@ -170,28 +170,28 @@
                     ry="2"></rect>
             </svg>
           </sea-link>
-          <sea-link class="tool tool-close"
-                    @action="doQuit">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round">
-              <line x1="18"
-                    y1="6"
-                    x2="6"
-                    y2="18"></line>
-              <line x1="6"
-                    y1="6"
-                    x2="18"
-                    y2="18"></line>
-            </svg>
+          <!--<sea-link class="tool tool-close"-->
+          <!--          @action="doQuit">-->
+          <!--  <svg-->
+          <!--    xmlns="http://www.w3.org/2000/svg"-->
+          <!--    width="24"-->
+          <!--    height="24"-->
+          <!--    viewBox="0 0 24 24"-->
+          <!--    fill="none"-->
+          <!--    stroke="currentColor"-->
+          <!--    stroke-linecap="round"-->
+          <!--    stroke-linejoin="round">-->
+          <!--    <line x1="18"-->
+          <!--          y1="6"-->
+          <!--          x2="6"-->
+          <!--          y2="18"></line>-->
+          <!--    <line x1="6"-->
+          <!--          y1="6"-->
+          <!--          x2="18"-->
+          <!--          y2="18"></line>-->
+          <!--  </svg>-->
 
-          </sea-link>
+          <!--</sea-link>-->
           <sea-link
             class="tool"
             :class="{ '-off': state.muteAudio }"
